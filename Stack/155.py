250773f6de65eef5af4ef448e0ba3bb8e808d300
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2024-03-09 14:32:14 -0500

    155

---
+++ Stack/155.py
@@ -0,0 +1,37 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# Implement the MinStack class:
+
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
+
+from collections import deque
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+        self.min_stack = deque()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.min_stack) == 0:
+            self.min_stack.append(val)
+        # edge case 0, 1, 0 need to keep two 0s in min_stack, basing on the pop logic
+        elif len(self.min_stack) > 0 and self.min_stack[-1] >= val:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if self.min_stack[-1] == val:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
