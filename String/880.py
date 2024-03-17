451d48717154004d799dec316122f0dd07f3bf67
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2024-03-17 19:21:43 -0400

    880

--- String/880.py
+++ String/880.py
@@ -1,1 +1,20 @@
 
+# You are given an encoded string s. To decode the string to a tape, the encoded string is read one character at a time and the following steps are taken:
+
+# If the character read is a letter, that letter is written onto the tape.
+# If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
+# Given an integer k, return the kth letter (1-indexed) in the decoded string.
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        l = 0
+        for i, c in enumerate(s):
+            if c.isdigit(): l *= int(c)
+            else: l += 1
+            if l >= k: break
+        for j in range(i, -1, -1):
+            if s[j].isdigit():
+                l /= int(s[j])
+                k %= l
+            else:
+                if k == 0 or k == l: return s[j]
+                l -= 1
