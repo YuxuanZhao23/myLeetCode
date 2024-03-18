41b7802a012dd591919b37c16521a3a7dbff3197
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2024-03-18 02:28:44 -0400

    1481

---
+++ Array/1481.py
@@ -0,0 +1,14 @@
+# Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+from ast import List
+from collections import Counter
+
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        count = Counter(arr)
+        for i, n in enumerate(sorted(count.values())):
+            if n <= k:
+                k -= n
+            else:
+                return len(count.keys()) - i
+        return 0
