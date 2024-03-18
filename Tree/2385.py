383aabe53c2f98f3d0bde544be7789baaad3e8ae
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2024-03-18 02:21:54 -0400

    2385

---
+++ Tree/2385.py
@@ -0,0 +1,42 @@
+# You are given the root of a binary tree with unique values, and an integer start. At minute 0, an infection starts from the node with value start.
+
+# Each minute, a node becomes infected if:
+
+# The node is currently uninfected.
+# The node is adjacent to an infected node.
+# Return the number of minutes needed for the entire tree to be infected.
+
+from collections import defaultdict, deque
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        adj = defaultdict(list)
+        q = deque([root])
+        while q:
+            n = q.popleft()
+            if n.left:
+                adj[n.val].append(n.left.val)
+                adj[n.left.val].append(n.val)
+                q.append(n.left)
+            if n.right:
+                adj[n.val].append(n.right.val)
+                adj[n.right.val].append(n.val)
+                q.append(n.right)
+
+        q = deque([(start, 0)])
+        res = 0
+        visited = set()
+        while q:
+            i, depth = q.popleft()
+            res = max(res, depth)
+            visited.add(i)
+            for a in adj[i]:
+                if a not in visited: q.append((a, depth + 1))
+        return res
